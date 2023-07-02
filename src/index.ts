6543d834231f97ae0ce6acc15836be3505073e16
joneubank <joneubank@gmail.com>
2023-07-02 11:58:03 -0400

    Export check matching filter

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * from './types/sqon';
+export { default as checkMatchingFilter } from './utils/checkMatchingFilter';
 export { default as reduceSQON } from './utils/reduceSQON';
 import { default as SQONBuilder } from './SQONBuilder';
 
